425e88fe24d265e204c32916f16b86f184f2da24
Daniel Andrews <andrewsdaniel999@hotmail.com>
2024-05-12 14:39:02 -0700

    testing merge checks

--- js/__testing__/unit.test.js
+++ js/__testing__/unit.test.js
@@ -1,6 +1,11 @@
 test("should be true", () => {
     expect(true).toBe(true);
 });
+
 test("should be false", () => {
     expect(false).toBe(false);
+});
+
+test("should be true", () => {
+    expect(1 == 1).toBe(true);
 });
