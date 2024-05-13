8082e3078697c9c824f203c96d8b05ffb975acbf
thedonutdan <thedonutdan@users.noreply.github.com>
2024-05-13 02:11:00 +0000

    Prettified Code!

--- js/testing-prettier.js
+++ js/testing-prettier.js
@@ -1,9 +1,10 @@
 /**
  * A function written sloppily to test Prettier workflow.
- * 
+ *
  * @returns {number} - Should return 3
  */
-function ugly() {  let a = 1;
+function ugly() {
+  let a = 1;
   let b = 2;
   return a + b;
 }
